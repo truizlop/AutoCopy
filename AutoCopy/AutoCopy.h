a5e7b7d184e3d426bea4d2efe2e58caef82aade8
Tomás Ruiz-López <trl.ironside@gmail.com>
2018-08-07 13:47:51 +0200

    Initial Commit

---
+++ AutoCopy/AutoCopy.h
@@ -0,0 +1,19 @@
+//
+//  AutoCopy.h
+//  AutoCopy
+//
+//  Created by Tomás Ruiz López on 7/8/18.
+//  Copyright © 2018 Sweetzpot AS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AutoCopy.
+FOUNDATION_EXPORT double AutoCopyVersionNumber;
+
+//! Project version string for AutoCopy.
+FOUNDATION_EXPORT const unsigned char AutoCopyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AutoCopy/PublicHeader.h>
+
+
